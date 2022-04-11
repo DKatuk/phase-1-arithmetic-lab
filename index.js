10f28061ad1d97f82ae63d73668aaa5d0f82b491
Dilara Katuk <dilarakatuk@gmail.com>
2022-04-12 00:04:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 =1;
+let num2 = 62;
+let multiply = num1*num2;
+console.log(multiply);
+
+let random =  Math.round(Math.random() > 0) ;
+
+var num3 = 15;
+var num4 = 11;
+var mod = num3 % num4;
+
+var max = Math.max(0,1,2,20);
